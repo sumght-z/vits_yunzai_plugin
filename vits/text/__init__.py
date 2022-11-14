156fd59cb6870aa13c3a5cd4cc79bf0820ad221d
sumght-z <54361600+sumght-z@users.noreply.github.com>
2022-11-14 17:14:41 +0800

    1

---
+++ vits/text/__init__.py
@@ -0,0 +1,60 @@
+""" from https://github.com/keithito/tacotron """
+from text import cleaners
+from text.symbols import symbols
+
+
+# Mappings from symbol to numeric ID and vice versa:
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+_id_to_symbol = {i: s for i, s in enumerate(symbols)}
+
+
+def text_to_sequence(text, cleaner_names):
+  '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+      text: string to convert to a sequence
+      cleaner_names: names of the cleaner functions to run the text through
+    Returns:
+      List of integers corresponding to the symbols in the text
+  '''
+  sequence = []
+
+  clean_text = _clean_text(text, cleaner_names)
+  return cleaned_text_to_sequence(clean_text)
+
+
+def cleaned_text_to_sequence(cleaned_text):
+  '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+      text: string to convert to a sequence
+    Returns:
+      List of integers corresponding to the symbols in the text
+  '''
+  sequence = []
+  for symbol in cleaned_text.split(" "):
+    if symbol in _symbol_to_id:
+      sequence.append(_symbol_to_id[symbol])
+    else:
+      for s in symbol:
+        sequence.append(_symbol_to_id[s])
+    sequence.append(_symbol_to_id[" "])
+  if sequence[-1] == _symbol_to_id[" "]:
+    sequence = sequence[:-1]
+  return sequence
+
+
+def sequence_to_text(sequence):
+  '''Converts a sequence of IDs back to a string'''
+  result = ''
+  for symbol_id in sequence:
+    s = _id_to_symbol[symbol_id]
+    result += s
+  return result
+
+
+def _clean_text(text, cleaner_names):
+  for name in cleaner_names:
+    cleaner = getattr(cleaners, name)
+    if not cleaner:
+      raise Exception('Unknown cleaner: %s' % name)
+    text = cleaner(text)
+  return text
