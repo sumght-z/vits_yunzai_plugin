db4560074385ee187a2569e9d2ba20078fc95a53
33335 <83267603+unknown10777@users.noreply.github.com>
2023-02-17 13:25:10 +0800

    忘了崩三的过滤了

--- apps/genshinSpeak.js
+++ apps/genshinSpeak.js
@@ -184,7 +184,7 @@ export class genshinSpeak extends plugin {
 	
 	//bh3音色 author：sumght
     else if (Object.keys(bh3Speakers).includes(data[0])){
-      data[1] = data[1].replace(/\\n/g,'。').replace(/\\r/g,'。').replace(/\ +/g, "，").replace(/[\r\n]/g, "。").replace("“", "，").replace("”", "，").replace(".", "。").replace(",", "，").replace("(", "，").replace(")", "，")
+      data[1] = data[1].replace(/\\n/g,'。').replace(/\\r/g,'。').replace(/\ +/g, "，").replace(/[\r\n]/g, "。").replace("“", "，").replace("”", "，").replace(".", "。").replace(",", "，").replace("(", "，").replace(")", "，").replace("&", "，").replace(/\\/g, '，').replace(/\//g, '，')
 	  let text = data[1].split("")
       const num = { "1": "一", "2": "二", "3": "三", "4": "四", "5": "五", "6": "六", "7": "七", "8": "八", "9": "九", "0": "零" }
       for (let i = 0; i < text.length; i++) {
